ee5026db7e7dafbb2b8cc24552c5dcdd68e1358c
odootask <odootask@gmail.com>
2023-12-20 09:40:52 +0530

    [ADD] Initial commit for odoo tasks

---
+++ task_auto_purchase/__manifest__.py
@@ -0,0 +1,15 @@
+# manifest.py
+
+{
+    'name': 'task auto purchase',
+    'version': '16.0.1.0.0',  # Change the version as needed
+    'summary': 'auto purchase vendor',
+    'category': 'work',
+    'author': 'Your Name',
+    'license': 'AGPL-3',
+    'depends': ['purchase','account','stock'],  # List of dependencies
+    'installable': True,
+    'data':[
+    ]
+
+}
